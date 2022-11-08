08a4edd057d8d9cace64d9e6ce55e47f2eb0adcc
dongsu <sonicce99@naver.com>
2022-11-08 11:56:06 +0900

    feat: About.tsx 생성

---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="aboutPage">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export const Head = () => <title>About Me</title>;
+
+// Step 3: Export your component
+export default AboutPage;
