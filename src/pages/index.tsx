c7d8d55844f06491b8d2d6e51b967f4b5b7ee80a
dongsu <sonicce99@naver.com>
2022-11-08 12:32:36 +0900

    feat: Local Image로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,7 @@ const IndexPage = () => {
     <Layout pageTitle="IndexPage">
       <main>
         <p>I'm making this by following the Gatsby Tutorial.</p>
-        <StaticImage
-          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-        />
+        <StaticImage alt="profile" src="../images/profile-pic.png" />
       </main>
     </Layout>
   );
